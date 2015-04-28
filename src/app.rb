943576531051969e4d273e7f249fa19cb9a130c5
ababup1192 <ababup1192@gmail.com>
2015-04-28 23:11:53 +0000

    Several GET methods

--- src/app.rb
+++ src/app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 
+# Sinatra Main controller
 class MainApp < Sinatra::Base
   # Sinatra Auto Reload
   configure :development do
@@ -8,5 +9,17 @@ class MainApp < Sinatra::Base
   end
   get '/' do
     'Hello, World!'
+  end
+  get '/echo/:word' do
+    "#{params['word']}"
+  end
+  get '/date' do
+    Time.now.to_s
+  end
+  get '/sum/:n1/:n2/:n3' do
+    (params['n1'].to_i + params['n2'].to_i + params['n3'].to_i).to_s
+  end
+  get '/reverse/:word' do
+    params['word'].reverse
   end
 end
