e7ab9b77ab77ca2509afc78e86ad4bd3cbe11fb9
ababup1192 <ababup1192@gmail.com>
2015-05-14 04:30:11 +0000

    Implement session

--- src/app.rb
+++ src/app.rb
@@ -2,11 +2,10 @@
 require 'sinatra/base'
 require 'sinatra/json'
 require 'sinatra/reloader'
-require 'data_mapper'
-require_relative 'word'
+# require 'data_mapper'
 
-DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default, 'postgres://vagrant:vagrant@localhost/myapp')
+# DataMapper::Logger.new($stdout, :debug)
+# DataMapper.setup(:default, 'postgres://vagrant:vagrant@localhost/myapp')
 
 # Sinatra Main controller
 class MainApp < Sinatra::Base
@@ -14,38 +13,15 @@ class MainApp < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
-  get '/words' do
-    json(Word.all)
+  # Session enable
+  use Rack::Session::Pool, expire_after: 2_592_000
+
+  get '/session' do
+    'value = ' << session[:value].inspect
   end
-  get '/words/:id' do
-    id = params[:id]
-    word = Word.get(id)
-    if word.nil?
-      json(error: "id:#{id} is not found.")
-    else
-      json(word)
-    end
-  end
-  post '/words' do
-    Word.create(msg: request.body.gets).id.to_json
-  end
-  put '/words/:id' do
-    id = params[:id]
-    word = Word.get(id)
-    if word.nil?
-      json(false)
-    else
-      word.update(msg: request.body.gets)
-      json(true)
-    end
-  end
-  delete '/words/:id' do
-    id = params[:id]
-    word = Word.get(id)
-    if word.nil?
-      json(false)
-    else
-      json(word.destroy)
-    end
+  post '/session' do
+    body = request.body.gets
+    session[:value] = body
+    body
   end
 end
